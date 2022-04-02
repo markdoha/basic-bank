03a2078b443d89a6fc2eba9be4a0353fb4cf0d0f
mark mamdouh <73199677+markdoha@users.noreply.github.com>
2022-04-02 21:55:36 +0200

    Update veiw.php
    final commit

--- veiw.php
+++ veiw.php
@@ -6,14 +6,33 @@ if (!$conn =  mysqli_connect("localhost", "id18456817_basicbank", "7%9zzK@T<u2DR
 $query = "select * from customers";
 $result = mysqli_query($conn, $query);
 if( isset($_POST['send']) ){
-    $query1 = "UPDATE `customers` SET balance=balance - '$_POST[amount]' where email='$_POST[sender]' ";
-    $query2 = "UPDATE `customers` SET balance= balance +'$_POST[amount]' where email='$_POST[reciever]' ";
-    $query1_run = mysqli_query($conn, $query1);
-    $query2_run = mysqli_query($conn, $query2);
-    $query3 = "INSERT INTO transactions(froom, too, amount) VALUES ('$_POST[sender]','$_POST[reciever]', '$_POST[amount]')";
-    $query3_run = mysqli_query($conn, $query3);
-    header('Location: ./veiw.php');
-     echo " <script type='text/javascript'>alert('success')</script>";
+    $main = "SELECT * from  `customers` where email ='$_POST[sender]'";
+    $main2 = "SELECT * from  `customers` where email='$_POST[reciever]'";
+    $main_run = mysqli_query($conn, $main);
+    $main2_run = mysqli_query($conn, $main2);
+    $fetched = mysqli_fetch_assoc($main_run);
+    $fetched2 = mysqli_fetch_assoc($main2_run);
+    $amount = "SELECT `balance` from `customers` where`customers` . `email` = '$_POST[sender]' ";
+    $amount_run = mysqli_query($conn, $amount);
+    $fetched3 = mysqli_fetch_assoc($amount_run);
+    
+    if( $fetched['email'] && $fetched2['email']){
+        if($fetched3['balance'] >= $_POST['amount'] ){
+        $query1 = "UPDATE `customers` SET balance=balance - '$_POST[amount]' where email='$_POST[sender]' ";
+        $query2 = "UPDATE `customers` SET balance= balance +'$_POST[amount]' where email='$_POST[reciever]' ";
+        $query1_run = mysqli_query($conn, $query1);
+        $query2_run = mysqli_query($conn, $query2);
+        $query3 = "INSERT INTO transactions(froom, too, amount) VALUES ('$_POST[sender]','$_POST[reciever]', '$_POST[amount]')";
+        $query3_run = mysqli_query($conn, $query3);
+        header('Location: ./veiw.php');
+        }
+        else{
+            echo " <script type='text/javascript'>alert('there is not enough money')</script>";
+        }
+    }
+    else{
+        echo "<script type='text/javascript'>alert('enter valid emails')</script>";
+    }
 }
 ?>
 <!DOCTYPE html>
